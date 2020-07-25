38dcae68aca3789db1b6bf2e04a89085a8e5c02f
Cody <ydoc118@gmail.com>
2020-07-25 18:36:34 -0400

    finished generateMarkdown function with markdown info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,49 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-     return `# ${data.title} 
-     ${data.description}
-     
+     return `# ${data.title}
+
+## Description:
+${data.description}
+
+## Table of Contents:
+
+* [Installation] (#installation)
+* [Information] (#info)
+* [License] (#license)
+* [Contributing] (#contributing)
+* [Tests] (#test)
+* [Questions] (#questions)
+
+## Installation
+
+To install all necessary dependencies, run the following command:
+
+" ${data.installation} "
+
+## Information
+
+${data.info}
+
+## License
+
+This project is licensed under the ${data.license} license.
+
+## Contributing
+
+To contribute to this repo, ${data.contributing}
+
+## Tests
+
+To run tests on the repo, run the following command:
+
+" ${data.test} "
+
+## Questions
+
+If you have any questions or comments about the repo, please feel free to email me at ${data.email},
+or visit my GitHub to see more of my work at [${data.username}](https://github.com/${data.username})
      `;
-  }
+}
   
   module.exports = generateMarkdown;
   
