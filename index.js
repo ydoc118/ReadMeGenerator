00bd5ae53353f903787f267d2a1ee9c51a79eb3b
Cody <ydoc118@gmail.com>
2020-07-25 14:30:45 -0400

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
+var inquirer = require("inquirer");
+
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "project"
+    },
+    {
+        type: "input",
+        message: "Please write a short description of your project",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "licence",
+        choices: [
+            "MIT",
+            "APACHES 2.0",
+            "GPL 3.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        default: "npm i",
+        name: "command"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        default: "npm test",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "info"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contribute"
+    },
+    
 
 ];
+inquirer.prompt(questions)
 
 // function to write README file
 function writeToFile(fileName, data) {
